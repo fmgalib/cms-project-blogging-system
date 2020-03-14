4e1ae21b5f3f4f932d504686868608fffc6ecfe3
fmgalib <fmgalib@gmail.com>
2020-03-14 22:52:14 +0600

    category added to sidebar

--- index.php
+++ index.php
@@ -16,6 +16,10 @@
             <!-- Blog Entries Column -->
 
             <div class="col-md-8">
+                <h1 class="page-header">
+                            Page Heading
+                        <small>Secondary Text</small>
+                            </h1>
 
 
 
@@ -35,10 +39,7 @@
 
                 ?>
 
-                        <h1 class="page-header">
-                            Page Heading
-                        <small>Secondary Text</small>
-                            </h1>
+                        
 
                                 <!-- First Blog Post -->
                                 <h2>
