a83b035f39036667090f637e437c469a8e6242a9
fmgalib <fmgalib@gmail.com>
2020-03-19 14:38:15 +0600

    category wise post page

--- includes/nav.php
+++ includes/nav.php
@@ -10,7 +10,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">My Blog</a>
+                <a class="navbar-brand" href="index.php">My Blog</a>
             </div>
 
 
