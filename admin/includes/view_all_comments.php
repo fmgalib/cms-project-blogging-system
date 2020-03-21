9b2a456ba1c275ee80ed98cd35d122321fcee4f4
fmgalib <fmgalib@gmail.com>
2020-03-21 14:38:15 +0600

    approving/unapproving comments

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -63,13 +63,37 @@
             }
 
             echo "<td>$comment_date</td>";
-            echo "<td><a href='posts.php?source=edit_post&p_id=' >Approve</a></td>";
-            echo "<td><a href='posts.php?delete='>Unapprove</a></td>";
+            echo "<td><a href='comments.php?approve=$comment_id' >Approve</a></td>";
+            echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
             echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
             echo "</tr>";
         }
 
         
+        // Approve Comments
+
+        if (isset($_GET['approve'])) {
+          $the_comment_id = $_GET['approve'];
+
+          $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id = '$the_comment_id'";
+          $delete_query = mysqli_query($connection, $query);
+          header("Location: comments.php");
+        }
+
+
+
+        // Unapprove Comments
+
+        if (isset($_GET['unapprove'])) {
+          $the_comment_id = $_GET['unapprove'];
+
+          $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id = '$the_comment_id'";
+          $delete_query = mysqli_query($connection, $query);
+          header("Location: comments.php");
+        }
+
+
+
         // Delete Comments
 
         if (isset($_GET['delete'])) {
