132f26f3ed411f35dff6967e06803db2c94bd6b8
fmgalib <fmgalib@gmail.com>
2020-03-23 23:25:05 +0600

    logout

---
+++ includes/logout.php
@@ -0,0 +1,15 @@
+
+<?php session_start(); ?>
+
+<?php  
+
+	$_SESSION['user_name'] 	= null;
+	$_SESSION['user_fname'] = null;
+	$_SESSION['user_lname'] = null;
+	$_SESSION['user_role'] 	= null;
+
+	header("Location: ../index.php");
+
+?>
+
+
