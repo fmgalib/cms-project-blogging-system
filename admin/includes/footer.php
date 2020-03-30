1dee5ab5856392876f086b02c3cdbacb78274b15
fmgalib <fmgalib@gmail.com>
2020-03-30 12:57:21 +0600

    registration form and real escape string

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -3,7 +3,6 @@
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
-    <script src="js/scripts.js"></script>
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
